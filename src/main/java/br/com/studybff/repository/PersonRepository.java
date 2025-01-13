81b2fdf89d731097f2a394729118b79826171df2
luciano-mota <amoosenhorjesus3@gmail.com>
2025-01-12 23:28:39 -0300

    Ajustes

--- src/main/java/br/com/studybff/repository/PersonRepository.java
+++ src/main/java/br/com/studybff/repository/PersonRepository.java
@@ -3,7 +3,6 @@ package br.com.studybff.repository;
 import br.com.studybff.entity.Person;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
